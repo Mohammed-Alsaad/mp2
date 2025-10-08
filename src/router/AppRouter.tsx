dbecd819fceb394a172c666d5c297f9dfc86734d
Mohammed Alsaad <malsaad3@illinois.edu>
2025-10-07 21:41:58 -0500

    fix

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,5 @@
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { SortProvider } from "../context/SortContext"; // make sure this path is correct
 import App from "../App";
 import ListView from "../pages/ListView/ListView";
 import GalleryView from "../pages/GalleryView/GalleryView";
@@ -7,14 +8,16 @@ import DetailView from "../pages/DetailView/DetailView";
 export default function AppRouter() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Navigate to="/list" replace />} />
-          <Route path="list" element={<ListView />} />
-          <Route path="gallery" element={<GalleryView />} />
-          <Route path="pokemon/:name" element={<DetailView />} />
-        </Route>
-      </Routes>
+      <SortProvider>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Navigate to="/list" replace />} />
+            <Route path="list" element={<ListView />} />
+            <Route path="gallery" element={<GalleryView />} />
+            <Route path="pokemon/:name" element={<DetailView />} />
+          </Route>
+        </Routes>
+      </SortProvider>
     </HashRouter>
   );
 }
